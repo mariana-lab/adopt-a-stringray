257ba043086da3f862d3c9d933ed4dc0ad9bc7d6
Mariana <mariana.barros@academiadecodigo.org>
2019-11-27 00:20:51 +0000

    add mock json of stringrays and vimdiesels

--- js/app/services/activity-service.js
+++ js/app/services/activity-service.js
@@ -1,16 +1,69 @@
 //responsible for making ajax requests
 define(function () {
-    internals = {};
-    externals = {};
+    var internals = {};
+    var externals = {};
 
-    externals.getName = function () {
-    }
+    externals.getName = function(){
+        return "lol";
+    };
 
     //ajax request
     //manipulate response
     //id, name and pic of activity
-    externals.findActivity = function (activityType, participants, price, cb, cberror) {
-    }
+    externals.findActivity = function(activityType, participants, price, cb, cberror) {
+    };
+
+    externals.getVimdieselsJson = function(){
+        return JSON.parse(
+            '{'+
+               '"vimdiesels": ['+
+               '{'+
+               '"name": "ElasVim",'+
+               '"nickname": "ondrej-tucek/elm-brunch-bulma-starter",'+
+               '"img": "https://cdn70.picsart.com/197965591000202.jpg?c256x256",' +
+               '"pitch": "vid/vid1.mp4",'+
+               '"description": "A simple Elm skeleton project with Bulma framework and Sass.",'+
+               '"orphan": "true",'+
+               '"vimdiesel": "none"'+
+               '},'+
+               '{' +
+               '"name": "EelesVim",'+
+               '"nickname": "ondrej-tucek/elm-brunch-bulma-starter",'+
+               '"img": "https://cdn70.picsart.com/197965591000202.jpg?c256x256",' +
+               '"pitch": "vid/vid1.mp4",'+
+               '"description": "A simple Elm skeleton project with Bulma framework and Sass.",'+
+               '"orphan": "true",'+
+               '"vimdiesel": "none"'+
+               '}'+
+                ']'+
+            '}');
+    };
+
+    externals.getStringRaysJson = function(){
+        return JSON.parse(
+            '{'+
+               '"stringrays": ['+
+               '{'+
+               '"name": "Elas",'+
+               '"nickname": "zeux",'+
+               '"img": "https://cdn70.picsart.com/197965591000202.jpg?c256x256",' +
+               '"pitch": "vid/vid1.mp4",'+
+               '"description": "A simple Elm skeleton project with Bulma framework and Sass.",'+
+               '"orphan": "true",'+
+               '"vimdiesel": "none"'+
+               '},'+
+               '{' +
+               '"name": "Eeles",'+
+               '"nickname": "zeux",'+
+               '"img": "https://cdn70.picsart.com/197965591000202.jpg?c256x256",' +
+               '"pitch": "vid/vid1.mp4",'+
+               '"description": "A simple Elm skeleton project with Bulma framework and Sass.",'+
+               '"orphan": "true",'+
+               '"vimdiesel": "none"'+
+               '}'+
+                ']'+
+            '}');
+    };
 
 
     return externals;
