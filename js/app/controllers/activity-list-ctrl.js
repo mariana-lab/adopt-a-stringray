2db4ca3ca714b6f60cb6ef94558870def90c7c02
Mariana <mariana.barros@academiadecodigo.org>
2019-11-27 00:24:38 +0000

    add initial controller

--- js/app/controllers/activity-list-ctrl.js
+++ js/app/controllers/activity-list-ctrl.js
@@ -1,30 +1,29 @@
 //every class starts with define, dependencies and a function
-define(['services/activity-service','services/img-service', 'views/activity-list-view'], function (actService, imgService, listActView) {
+define(["services/activity-service", "views/activity-list-view"], function( actService, listActView) {
+  var internals = {};
+  var externals = {};
 
-    var internals = {};
-    var externals = {};
+  externals.start = function() {
+    listActView.render(actService.getVimdieselsJson() ,actService.getStringRaysJson());
+    bindEvents();
+  };
 
-    externals.start = function(){
-        
-       
-        listActView.render();
-        bindEvents();
-    
-    };
+  function bindEvents() {
+    //for each event on the page give a handler
+    listActView.bind("adoptButtonPush", adoptHandler);
+  }
 
-    function bindEvents(){
-       
+  function adoptHandler(adoption) {
+    //adoption.vimdieselname for checking the promotioncode
+    //adoption.promotioncode for checkin if its possible to update
+    //adoption.stringrayname for updating
+  }
 
-    }
+  function buttonHandler() {}
 
-    function buttonHandler(){
-    }
+  function formHandler(activityType, participants, price) {}
 
-    function formHandler(activityType, participants, price){
-    }
+  function listHandler() {}
 
-    function listHandler(){
-    }
-
-    return externals;
+  return externals;
 });
--- js/app/services/activity-service.js
+++ js/app/services/activity-service.js
@@ -67,4 +67,4 @@ define(function () {
 
 
     return externals;
-});
+})
